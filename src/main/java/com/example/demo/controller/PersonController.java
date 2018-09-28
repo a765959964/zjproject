44533c9438d2924fc35dadc3e0fd56e07c45adbd
zhang765959964 <zhang765959964@qq.com>
2018-09-28 17:06:51 +0800

    添加layui iframe后台模板文件，实现增删改查  小demo

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -1,14 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.core.ret.LayuiResult;
-import com.example.demo.core.ret.RetResult;
 import com.example.demo.core.ret.RetResponse;
-import com.example.demo.core.utils.ApplicationUtils;
+import com.example.demo.core.ret.RetResult;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -18,7 +20,7 @@ import java.util.List;
 * @author zf
 * @date 2018/09/26 16:23
 */
-@RestController
+@Controller
 @RequestMapping("/person")
 public class PersonController {
 
@@ -26,37 +28,54 @@ public class PersonController {
     private PersonService personService;
 
     @PostMapping("/insert")
+    @ResponseBody
     public RetResult<Integer> insert(Person person) throws Exception{
-		person.setId(Long.valueOf(ApplicationUtils.getUUID()));
+//		person.setId(Long.valueOf(ApplicationUtils.getUUID()));
     	Integer state = personService.insert(person);
         return RetResponse.makeOKRsp(state);
     }
 
     @PostMapping("/deleteById")
+    @ResponseBody
     public RetResult<Integer> deleteById(@RequestParam String id) throws Exception {
         Integer state = personService.deleteById(id);
         return RetResponse.makeOKRsp(state);
     }
 
     @PostMapping("/update")
+    @ResponseBody
     public RetResult<Integer> update(Person person) throws Exception {
         Integer state = personService.update(person);
         return RetResponse.makeOKRsp(state);
     }
 
     @PostMapping("/selectById")
+    @ResponseBody
     public RetResult<Person> selectById(@RequestParam String id) throws Exception {
         Person person = personService.selectById(id);
         return RetResponse.makeOKRsp(person);
     }
 
+    @RequestMapping(value = "/getById",method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView getById(String id, Model model) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        Person person = personService.selectById(id);
+        mv.setViewName("views/person/personEdit");
+        mv.addObject("person",person);
+        return mv;
+    }
+
+
+
     /**
 	* @Description: 分页查询
 	* @param page 页码
-	* @param size 每页条数
+	* @param limit 每页条数
 	* @Reutrn RetResult<PageInfo<Person>>
 	*/
     @PostMapping("/list")
+    @ResponseBody
     public RetResult<PageInfo<Person>> list(@RequestParam(defaultValue = "0") Integer page,
 					@RequestParam(defaultValue = "0") Integer limit) throws Exception {
         PageHelper.startPage(page, limit);
